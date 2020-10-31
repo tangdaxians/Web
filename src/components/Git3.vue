4331987f424dc896555f36e94a913668a3273c29
tangdaxians <tangdaxians@qq.com>
2020-10-31 11:31:40 +0800

    Git3

---
+++ src/components/Git3.vue
@@ -0,0 +1,25 @@
+<template>
+	<div class="hello">
+		<h1>{{ msg }}</h1>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'HelloWorld',
+	data () {
+		return {
+			msg: 'Git 3'
+		}
+	}
+}
+</script>
+
+<style scoped>
+.hello {
+	width: 100%;
+	height: auto;
+	text-align: center;
+	line-height: 200px;
+}
+</style>
