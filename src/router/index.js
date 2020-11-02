8c86899bdab8a47b1547e269e19daf56266aba0a
tangdaxians <tangdaxians@qq.com>
2020-11-02 11:32:10 +0800

    +c - 4

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,31 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
-import vr from "./modules/vant"
-import er from "./modules/element"
-
 Vue.use(Router)
 
 const originalPush = Router.prototype.push
 	Router.prototype.push = function push(location) {
 	return originalPush.call(this, location).catch(err => err)
 }
+
+
+const Routers = [];
+
+const e = require.context('@/components/Element', false, /\.vue$/)
+const v = require.context('@/components/Vant', false, /\.vue$/)
+
+e.keys().map(e).forEach(i=>{
+	var name = i.default.name;
+	Routers.push({
+		path: name, name, component: resolve => require(['@/components/Element/'+name.slice(1)], resolve),
+	});
+})
+v.keys().map(v).forEach(i=>{
+	var name = i.default.name;
+	Routers.push({
+		path: name, name, component: resolve => require(['@/components/Vant/'+name.slice(1)], resolve),
+	});
+})
 
 export default new Router({
 	mode:"history",
@@ -18,7 +34,7 @@ export default new Router({
 			path: '/',
 			name: 'Index',
 			component: resolve => require(['@/components/index'], resolve),
-			children:[ ...vr, ...er ]
+			children:[...Routers]
 		}
 	]
 })
--- src/router/modules/vant.js
+++
@@ -1,9 +0,0 @@
-const vantRouter = [
-	{
-		path:"vbutton",
-		name:"vbutton",
-		component:resolve => require(['@/components/Vant/button'], resolve),
-	},
-]
-
-export default vantRouter
